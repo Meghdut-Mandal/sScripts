4970239db5330b77f9a95d776398d6ea9c5c76a0
Meghdut Mandal <meghdut.windows@gmail.com>
2020-12-06 12:25:21 +0530

    Added Office 19 installer

---
+++ Office_Install.bat
@@ -0,0 +1,20 @@
+@echo off
+
+whoami /groups | find "S-1-16-12288" > nul
+
+if %errorlevel% == 0 (
+ echo Welcome, To Office 19 Installer
+) else (
+ echo Run this script as administrator.
+ pause
+)
+
+echo Installing Chocolatey 
+
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco feature enable -n=allowGlobalConfirmation
+
+echo Instaling Office 19 
+choco install office2019-proplus --pre
