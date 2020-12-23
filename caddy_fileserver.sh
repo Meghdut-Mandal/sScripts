cda52f429312cd7f607634778287833091f3abba
Meghdut Mandal <meghdut.windows@gmail.com>
2020-12-23 12:51:05 +0530

    Create caddy_fileserver.sh

---
+++ caddy_fileserver.sh
@@ -0,0 +1,7 @@
+echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" \
+    | sudo tee -a /etc/apt/sources.list.d/caddy-fury.list
+sudo apt update
+sudo apt install caddy -y 
+
+caddy file-server -root /home/ubuntu/ -listen 0.0.0.0:8000 -browse 
+echo Caddy FileServer at http://"$(dig +short myip.opendns.com @resolver1.opendns.com)":8000
