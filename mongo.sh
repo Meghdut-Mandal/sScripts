c87f890e780706c1514e1da8e85d3e5032112ef1
Meghdut Mandal <meghdut.windows@gmail.com>
2020-12-28 20:32:11 +0530

    Update mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -1,6 +1,7 @@
 tee /etc/apt/sources.list.d/mongodb-org-4.2.list << EOF
 deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse
 EOF
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 apt-get update
