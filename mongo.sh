cd4bcd71172cf6c7e389a4b1f97bab786070cd48
Meghdut Mandal <meghdut.windows@gmail.com>
2020-12-28 20:29:34 +0530

    Update mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -1,9 +1,9 @@
-sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list << EOF
+tee /etc/apt/sources.list.d/mongodb-org-4.2.list << EOF
 deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse
 EOF
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo apt-get update
-sudo apt-get --assume-yes install mongodb-server
-sudo systemctl start  mongodb
-sudo systemctl enable mongodb
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+apt-get update
+apt-get --assume-yes install mongodb-server
+systemctl start  mongodb
+systemctl enable mongodb
