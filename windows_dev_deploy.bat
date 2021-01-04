6c6c39b9e2024bca3fa3c5ce80aa958714c7fd76
Meghdut Mandal <meghdut.windows@gmail.com>
2021-01-05 00:12:06 +0530

    Update windows_dev_deploy.bat

--- windows_dev_deploy.bat
+++ windows_dev_deploy.bat
@@ -1,1 +1,2 @@
-install yarn webstorm intellijidea-ultimate nodejs
+Invoke-WebRequest -OutFile comm.bat -Uri https://github.com/Meghdut-Mandal/sScripts/raw/main/windows_deploy.bat; .\comm.bat ;
+choco install yarn webstorm intellijidea-ultimate nodejs
