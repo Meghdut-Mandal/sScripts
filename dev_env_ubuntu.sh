8d6a345b0e2bb8ba2bb76f31f3b1570b8f8288fe
Meghdut Mandal <meghdut.windows@gmail.com>
2021-04-06 22:17:06 +0530

    Update dev_env_ubuntu.sh

--- dev_env_ubuntu.sh
+++ dev_env_ubuntu.sh
@@ -1,6 +1,6 @@
 sudo apt -y install axel aria2
 cd /home/
-sudo axel -a -n 3 https://ah.potassium.workers.dev/0:/dat.gz 
+sudo axel  -n 3 https://ah.potassium.workers.dev/0:/dat.gz 
 sudo tar -xzf dat.gz 
 sudo rm dat.gz
 
