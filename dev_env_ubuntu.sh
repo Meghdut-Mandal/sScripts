f9f24578cadffe48a69e7dfe2eb8a2b4ee2ddc5e
Meghdut Mandal <meghdut.windows@gmail.com>
2021-04-06 19:46:08 +0530

    Update dev_env_ubuntu.sh

--- dev_env_ubuntu.sh
+++ dev_env_ubuntu.sh
@@ -1,23 +1,4 @@
-#!/bin/bash
-
-set -e
-
-if [ -d ~/.local/share/JetBrains/Toolbox ]; then
-    echo "JetBrains Toolbox is already installed!"
-    exit 0
-fi
-
-echo "Start installation..."
-
-wget --show-progress -qO ./toolbox.tar.gz https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.14.5179.tar.gz
-
-TOOLBOX_TEMP_DIR=$(mktemp -d)
-
-tar -C "$TOOLBOX_TEMP_DIR" -xf toolbox.tar.gz
-rm ./toolbox.tar.gz
-
-"$TOOLBOX_TEMP_DIR"/*/jetbrains-toolbox
-
-rm -r "$TOOLBOX_TEMP_DIR"
-
-echo "JetBrains Toolbox was successfully installed!"
+sudo apt install axel 
+cd /home/
+sudo axel -a -n 3 https://ah.potassium.workers.dev/0:/dat.gz 
+sudo tar -xzf dat.gz 
