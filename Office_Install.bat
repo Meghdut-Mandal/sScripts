88d22b56efde349e2302513433bde671e4589ab6
Meghdut Mandal <meghdut.windows@gmail.com>
2021-05-13 10:56:18 +0530

    Update Office_Install.bat

--- Office_Install.bat
+++ Office_Install.bat
@@ -1,3 +1,12 @@
 echo This script is no longer Maintained contact meghdut, for the office deployment tool.
 
+echo Activating Office 
+cd /d %ProgramFiles%\Microsoft Office\Office16
+for /f %x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /unpkey:6MWKP >nul
+cscript ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
+cscript ospp.vbs /sethst:kms8.msguides.com
+cscript ospp.vbs /act
+
 PAUSE
